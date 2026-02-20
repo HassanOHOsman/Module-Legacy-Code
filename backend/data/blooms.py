6cf28727298348a0a1c2ad6b121bbddd9c5b0187
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-20 21:03:42 +0000

    Get AS bloom ID from the error on on console. Use it to skip of it when blooms fetched from backend

--- backend/data/blooms.py
+++ backend/data/blooms.py
@@ -6,6 +6,8 @@ from typing import Any, Dict, List, Optional
 from data.connection import db_cursor
 from data.users import User
 
+AS_BLOOM_ID = 1771113371599478
+
 
 @dataclass
 class Bloom:
@@ -69,6 +71,8 @@ def get_blooms_for_user(
         blooms = []
         for row in rows:
             bloom_id, sender_username, content, timestamp = row
+            if bloom_id == AS_BLOOM_ID:
+                continue
             blooms.append(
                 Bloom(
                     id=bloom_id,
@@ -122,6 +126,8 @@ def get_blooms_with_hashtag(
         blooms = []
         for row in rows:
             bloom_id, sender_username, content, timestamp = row
+            if bloom_id == AS_BLOOM_ID:
+                continue
             blooms.append(
                 Bloom(
                     id=bloom_id,
