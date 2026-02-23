de332cbbc529aa4ea5fde9c2ee550ea12325c7cc
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-23 11:09:42 +0000

    async function handleFollow so that a follow/unfloow action is determined by the text in the follow/unfollow btn

--- front-end/components/profile.mjs
+++ front-end/components/profile.mjs
@@ -27,7 +27,7 @@ function createProfile(template, {profileData, whoToFollow, isLoggedIn}) {
   followerCountEl.textContent = profileData.followers?.length || 0;
   followingCountEl.textContent = profileData.follows?.length || 0;
   followButtonEl.setAttribute("data-username", profileData.username || "");
-  followButtonEl.hidden = profileData.is_self || profileData.is_following;
+  followButtonEl.hidden = profileData.is_self;
   followButtonEl.addEventListener("click", handleFollow);
   if (!isLoggedIn) {
     followButtonEl.style.display = "none";
@@ -62,7 +62,13 @@ async function handleFollow(event) {
   const username = button.getAttribute("data-username");
   if (!username) return;
 
-  await apiService.followUser(username);
+  if (button.textContent === "Follow") {
+    await apiService.followUser(username);
+  } else {
+    await apiService.unfollowUser(username);
+  }
+
+  
   await apiService.getWhoToFollow();
 }
 
