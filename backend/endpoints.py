05ad7279c3fac2695686aa2331aaa306bf83da99
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-21 18:13:57 +0100

    add an endpoint for reblooming

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -245,3 +245,25 @@ def verify_request_fields(names_to_types: Dict[str, type]) -> Union[Response, No
                 )
             )
     return None
+
+
+@jwt_required()
+def rebloom(id_str):
+    try:
+        original_bloom_id = int(id_str)
+    except ValueError:
+        return make_response(("Invalid bloom id", 400))
+
+    current_user = get_current_user()
+
+    original_bloom = blooms.get_bloom(original_bloom_id)
+
+    if original_bloom is None:
+        return make_response(("Bloom not found", 404))
+
+    rebloomed = blooms.rebloom(
+        sender=current_user,
+        original_bloom_id=original_bloom_id,
+    )
+
+    return jsonify(rebloomed)
